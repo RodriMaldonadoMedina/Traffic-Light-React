c371275605a24266e1fec2ff5dacfaa279fd0a40
RodriMaldonadoMedina <maldonadomedinarodrigo@gmail.com>
2022-08-13 11:57:57 +0000

    las luces del semaforo andan segun los click que se les hagan

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from "react";
 
-const [color, setColor] = useState("rojo");
-
 //create your first component
 const Semaforo = () => {
+	const [color, setColor] = useState("");
+
+	useEffect(()=>{
+		setColor("rojo")
+	},[])
+
+	const cambiarLuzRoja = ()=>{
+		setColor("rojo")
+	}
+
+	const cambiarLuzAmarilla = ()=>{
+		setColor("amarillo")
+	}
+
+	const cambiarLuzVerde = ()=>{
+		setColor("verde")
+	}
+
 	return (
 		<div className="container">
 			<div className="caja d-flex">
-				<div className={"rojo"+((color === "rojo") ? " brilla" : "")}></div>
-				<div className="amarillo"></div>
-				<div className="verde"></div>
+				<div className={"rojo"+((color === "rojo") ? " brilla" : "")} onClick={cambiarLuzRoja}></div>
+				<div className={"amarillo"+((color === "amarillo") ? " brilla" : "")} onClick={cambiarLuzAmarilla}></div>
+				<div className={"verde"+((color === "verde") ? " brilla" : "")} onClick={cambiarLuzVerde}></div>
 			</div>
 		</div>
 	);
