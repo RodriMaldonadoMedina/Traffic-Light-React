379ba1966e39898f5d4f0a69469eeab929d2659f
RodriMaldonadoMedina <maldonadomedinarodrigo@gmail.com>
2022-08-14 10:27:41 +0000

    Se agrego logica de cuando parar el setInterval

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from "react";
 //create your first component
 const Semaforo = () => {
 	const [color, setColor] = useState("");
+	const [encendido, setEncendido] = useState(false);
+	const colores = ["rojo", "amarillo", "verde"];
+	const [intervalo, setIntervalo] = useState(null);
 
-	useEffect(()=>{
-		setColor("rojo")
-	},[])
-
+	//Como seria para poder reutilizar la funcion sin necesidad de expresarla para cada color?
 	const cambiarLuzRoja = ()=>{
 		setColor("rojo")
 	}
@@ -19,14 +19,82 @@ const Semaforo = () => {
 	const cambiarLuzVerde = ()=>{
 		setColor("verde")
 	}
+	
+	//funcion que para el setInterval con el numero de intervalo
+	const stopInterval = ()=>{
+		clearInterval(intervalo);
+		setIntervalo(null);
+	}
+
+	//funcion que me implementa la clase brilla en cualquier luz
+	const setBrillo = (colorLuz)=>{
+		let clase = "";
+		if (color === colorLuz) 
+			clase += " brilla";
+		return clase;
+	}
+/*
+	//timeouts para que vayan en orden
+	const timeout3 = ()=>{
+		setColor("verde");
+		setTimeout(console.log("estoy en el ultimo timeout"),1000)
+	} 
+
+	const timeout2 = ()=>{
+		setColor("amarillo");
+		console.log("entro al segundo timeout");
+		let timeOut2 = setTimeout(timeout3(),1000);
+		console.log("salgo del segundo timeout");
+		clearTimeout(timeOut2);
+	}
+
+	const enOrden = ()=>{
+		setColor("rojo");
+		console.log("entro al primer timeout");
+		let timeOut1 = setTimeout(timeout2(),1000);
+		console.log("salgo del primer timeout");
+		clearTimeout(timeOut1);
+	}
+*/
+	const lucesParty = () =>{
+		setInterval(() =>{
+			let aleatorio = colores[Math.floor(Math.random()* colores.length)]
+			//enOrden();
+			console.log(aleatorio)
+			setColor(aleatorio)
+		},1000)
+	}
+
+	const encenderSemaforo = ()=>{
+		if (!encendido){
+			setEncendido(true);
+			//lucesParty();
+		}
+		else setEncendido(false);
+	}
+
+	useEffect(()=>{
+		if (!intervalo){
+			let newIntervalo = setInterval(() => {
+				let aleatorio = colores[Math.floor(Math.random()* colores.length)];
+				//enOrden();
+				console.log(aleatorio);
+				setColor(aleatorio);
+			}, 1000);
+			setIntervalo(newIntervalo);
+		}
+		else
+			stopInterval();
+	},[encendido])
 
 	return (
 		<div className="container">
-			<div className="caja d-flex">
-				<div className={"rojo"+((color === "rojo") ? " brilla" : "")} onClick={cambiarLuzRoja}></div>
-				<div className={"amarillo"+((color === "amarillo") ? " brilla" : "")} onClick={cambiarLuzAmarilla}></div>
-				<div className={"verde"+((color === "verde") ? " brilla" : "")} onClick={cambiarLuzVerde}></div>
+			<div className="caja d-flex mt-5">
+				<div className={"rojo"+(setBrillo("rojo"))} onClick={cambiarLuzRoja}></div>
+				<div className={"amarillo"+(setBrillo("amarillo"))} onClick={cambiarLuzAmarilla}></div>
+				<div className={"verde"+(setBrillo("verde"))} onClick={cambiarLuzVerde}></div>
 			</div>
+			<button className="btn btn-primary my-5" onClick={encenderSemaforo}>Encender o Apagar el Semaforo</button>
 		</div>
 	);
 };
